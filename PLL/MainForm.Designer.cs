b069948f28d86e09207a99ab5826a6566d15ebf3
letientrung223 <khanhky245@gmail.com>
2020-06-28 15:46:26 +0700

    Add project files.

---
+++ PLL/MainForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace DA_CNPM.PLL
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bt_SignUp = new System.Windows.Forms.Button();
+            this.bt_Signin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_TheLoai = new System.Windows.Forms.ComboBox();
+            this.bt_Search = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lb_SachOnline = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.llb_Intro = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.bt_SignUp);
+            this.panel1.Controls.Add(this.bt_Signin);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cb_TheLoai);
+            this.panel1.Controls.Add(this.bt_Search);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.lb_SachOnline);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1418, 60);
+            this.panel1.TabIndex = 1;
+            // 
+            // bt_SignUp
+            // 
+            this.bt_SignUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.bt_SignUp.Location = new System.Drawing.Point(1287, 8);
+            this.bt_SignUp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.bt_SignUp.Name = "bt_SignUp";
+            this.bt_SignUp.Size = new System.Drawing.Size(112, 40);
+            this.bt_SignUp.TabIndex = 6;
+            this.bt_SignUp.Text = "Sign up";
+            this.bt_SignUp.UseVisualStyleBackColor = false;
+            // 
+            // bt_Signin
+            // 
+            this.bt_Signin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.bt_Signin.Location = new System.Drawing.Point(1166, 8);
+            this.bt_Signin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.bt_Signin.Name = "bt_Signin";
+            this.bt_Signin.Size = new System.Drawing.Size(112, 40);
+            this.bt_Signin.TabIndex = 5;
+            this.bt_Signin.Text = "Sign in";
+            this.bt_Signin.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1062, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Category";
+            // 
+            // cb_TheLoai
+            // 
+            this.cb_TheLoai.FormattingEnabled = true;
+            this.cb_TheLoai.Location = new System.Drawing.Point(838, 12);
+            this.cb_TheLoai.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cb_TheLoai.Name = "cb_TheLoai";
+            this.cb_TheLoai.Size = new System.Drawing.Size(212, 28);
+            this.cb_TheLoai.TabIndex = 3;
+            // 
+            // bt_Search
+            // 
+            this.bt_Search.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.bt_Search.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.bt_Search.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.bt_Search.Location = new System.Drawing.Point(717, 8);
+            this.bt_Search.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.bt_Search.Name = "bt_Search";
+            this.bt_Search.Size = new System.Drawing.Size(112, 42);
+            this.bt_Search.TabIndex = 2;
+            this.bt_Search.Text = "Search";
+            this.bt_Search.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(339, 8);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(367, 39);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lb_SachOnline
+            // 
+            this.lb_SachOnline.AutoSize = true;
+            this.lb_SachOnline.Font = new System.Drawing.Font("MV Boli", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_SachOnline.ForeColor = System.Drawing.Color.Red;
+            this.lb_SachOnline.Location = new System.Drawing.Point(118, 8);
+            this.lb_SachOnline.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_SachOnline.Name = "lb_SachOnline";
+            this.lb_SachOnline.Size = new System.Drawing.Size(187, 41);
+            this.lb_SachOnline.TabIndex = 0;
+            this.lb_SachOnline.Text = "SachOnline";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel4.Controls.Add(this.llb_Intro);
+            this.panel4.Location = new System.Drawing.Point(3, 660);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1418, 60);
+            this.panel4.TabIndex = 4;
+            // 
+            // llb_Intro
+            // 
+            this.llb_Intro.AutoSize = true;
+            this.llb_Intro.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.llb_Intro.Location = new System.Drawing.Point(33, 14);
+            this.llb_Intro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.llb_Intro.Name = "llb_Intro";
+            this.llb_Intro.Size = new System.Drawing.Size(137, 29);
+            this.llb_Intro.TabIndex = 0;
+            this.llb_Intro.TabStop = true;
+            this.llb_Intro.Text = "Introduction";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1430, 724);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bt_SignUp;
+        private System.Windows.Forms.Button bt_Signin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb_TheLoai;
+        private System.Windows.Forms.Button bt_Search;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lb_SachOnline;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.LinkLabel llb_Intro;
+    }
+}
---
+++ PLL/SignIn.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace DA_CNPM.PLL
+{
+    partial class SignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_Password = new System.Windows.Forms.Label();
+            this.lb_UserName = new System.Windows.Forms.Label();
+            this.bt_SignIn = new System.Windows.Forms.Button();
+            this.tb_PassWord = new System.Windows.Forms.TextBox();
+            this.tb_UserName = new System.Windows.Forms.TextBox();
+            this.lb_SachOnline = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_Password
+            // 
+            this.lb_Password.AutoSize = true;
+            this.lb_Password.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Password.Location = new System.Drawing.Point(13, 130);
+            this.lb_Password.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_Password.Name = "lb_Password";
+            this.lb_Password.Size = new System.Drawing.Size(111, 29);
+            this.lb_Password.TabIndex = 12;
+            this.lb_Password.Text = "Password";
+            // 
+            // lb_UserName
+            // 
+            this.lb_UserName.AutoSize = true;
+            this.lb_UserName.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_UserName.Location = new System.Drawing.Point(13, 93);
+            this.lb_UserName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_UserName.Name = "lb_UserName";
+            this.lb_UserName.Size = new System.Drawing.Size(125, 29);
+            this.lb_UserName.TabIndex = 11;
+            this.lb_UserName.Text = "User Name";
+            // 
+            // bt_SignIn
+            // 
+            this.bt_SignIn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(184)))), ((int)(((byte)(255)))));
+            this.bt_SignIn.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_SignIn.ForeColor = System.Drawing.Color.White;
+            this.bt_SignIn.Location = new System.Drawing.Point(163, 189);
+            this.bt_SignIn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.bt_SignIn.Name = "bt_SignIn";
+            this.bt_SignIn.Size = new System.Drawing.Size(112, 45);
+            this.bt_SignIn.TabIndex = 10;
+            this.bt_SignIn.Text = "Sign in";
+            this.bt_SignIn.UseVisualStyleBackColor = false;
+            // 
+            // tb_PassWord
+            // 
+            this.tb_PassWord.Location = new System.Drawing.Point(187, 133);
+            this.tb_PassWord.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tb_PassWord.Name = "tb_PassWord";
+            this.tb_PassWord.Size = new System.Drawing.Size(232, 26);
+            this.tb_PassWord.TabIndex = 9;
+            // 
+            // tb_UserName
+            // 
+            this.tb_UserName.Location = new System.Drawing.Point(187, 93);
+            this.tb_UserName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tb_UserName.Name = "tb_UserName";
+            this.tb_UserName.Size = new System.Drawing.Size(232, 26);
+            this.tb_UserName.TabIndex = 8;
+            // 
+            // lb_SachOnline
+            // 
+            this.lb_SachOnline.AutoSize = true;
+            this.lb_SachOnline.Font = new System.Drawing.Font("MV Boli", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_SachOnline.ForeColor = System.Drawing.Color.Red;
+            this.lb_SachOnline.Location = new System.Drawing.Point(140, 18);
+            this.lb_SachOnline.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_SachOnline.Name = "lb_SachOnline";
+            this.lb_SachOnline.Size = new System.Drawing.Size(187, 41);
+            this.lb_SachOnline.TabIndex = 7;
+            this.lb_SachOnline.Text = "SachOnline";
+            // 
+            // SignIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(438, 263);
+            this.Controls.Add(this.lb_Password);
+            this.Controls.Add(this.lb_UserName);
+            this.Controls.Add(this.bt_SignIn);
+            this.Controls.Add(this.tb_PassWord);
+            this.Controls.Add(this.tb_UserName);
+            this.Controls.Add(this.lb_SachOnline);
+            this.Name = "SignIn";
+            this.Text = "SignIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_Password;
+        private System.Windows.Forms.Label lb_UserName;
+        private System.Windows.Forms.Button bt_SignIn;
+        private System.Windows.Forms.TextBox tb_PassWord;
+        private System.Windows.Forms.TextBox tb_UserName;
+        private System.Windows.Forms.Label lb_SachOnline;
+    }
+}
